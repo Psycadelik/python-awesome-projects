f90600e0a5e16383531e9cacb8276ecff2451168
Psycadelik <Francisadrian9@hotmail.com>
2018-09-13 06:43:12 -0400

    calculationg similarity ratio and optimizing final output

--- PythonInteractiveDictionary/dictionary.py
+++ PythonInteractiveDictionary/dictionary.py
@@ -1,5 +1,7 @@
 import os
 import json
+import difflib
+from difflib import get_close_matches
 
 data = json.load(open("data.json",'r'))
 
@@ -7,12 +9,31 @@ def translate(word):
 	word=word.lower()
 	if word in data:
 		return data[word]
+	elif word.title() in data:
+		return data[word.title()]
+	elif word.upper() in data:
+		return data[word.upper()]
+	elif len(get_close_matches(word, data.keys())) > 0:
+		yn = input("Did you mean %s instead? Enter Y if yes, or N if no." % get_close_matches(word, data.keys())[0])
+
+		if yn == "Y":
+			return data[get_close_matches(word, data.keys())[0]]
+		elif yn == "N":
+			return "Your word does not exist. Please double check and try again"
+		else:
+			return "We didn't quite catch that :(. Please input Y or N "
 	else:
 		return "Your word does not exist. Please double check and try again"
 
 wordInput = input("Please input the word you want to search for: ")
 
-print (translate(wordInput))
+output = translate(wordInput)
+
+if type(output) == list:
+	for item in output:
+		print(item)
+else:
+	print(output)		
 
 
 
